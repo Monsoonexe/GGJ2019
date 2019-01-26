814c7beb2905397b54444d2bce883593984a2c95
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-01-26 10:36:31 -0800

    fixed name

---
+++ Game#1/Assets/Scripts/GrapplingHookBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class GrapplingHookBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform grappleOriginTransform;
+
+    private LineRenderer lineRenderer;
+    private readonly string grapplingHookButton = "GrapplingHook";
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>() as LineRenderer;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GrapplingHook()
+    {
+
+    }
+
+    
+}
