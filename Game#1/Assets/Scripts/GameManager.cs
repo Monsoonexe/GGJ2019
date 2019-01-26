f159e2a58d2ec64eea4c2b69c0917daaff84e2f0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-01-26 15:35:19 -0800

    Update GameManager.cs
    new functions for resapwn

--- Game#1/Assets/Scripts/GameManager.cs
+++ Game#1/Assets/Scripts/GameManager.cs
@@ -5,10 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager gameManager;
+    private static int sceneNo = 0;
+
     [SerializeField] private bool TESTING = false;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private int winCelebrateSeconds = 5;
+
+    private Vector3 nextSpawnPoint;
+
+    private void Awake()
+    {
+        SingletonPattern();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+
+        nextSpawnPoint = startPoint.position;
         
     }
 
@@ -23,5 +38,49 @@ public class GameManager : MonoBehaviour
             }
         }
         
+    }
+
+    private void SingletonPattern()
+    {
+        if (!gameManager)
+        {
+            gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>() as GameManager;
+            DontDestroyOnLoad(this.gameObject);//IMMORTALITY!!!!
+        }
+        else
+        {
+            Destroy(this.gameObject);//THERE CAN ONLY BE ONE!!!!
+        }
+    }
+
+    public void WinLevel()
+    {
+        //say congrats!
+
+        //do win animations
+        StartCoroutine(LoadLevelAfterDelay(winCelebrateSeconds));
+
+    }
+
+    /// <summary>
+    /// Wait for some seconds for animations, then load next level
+    /// </summary>
+    /// <returns></returns>
+    private static IEnumerator LoadLevelAfterDelay(int delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+
+        //load next level
+        SceneManager.LoadScene(++sceneNo);
+    }
+
+    public void UpdateCheckPoint(Vector3 newCheckPoint)
+    {
+        nextSpawnPoint = newCheckPoint;
+    }
+
+    public Vector3 GetSpawnPoint()
+    {
+        return nextSpawnPoint;
     }
 }
