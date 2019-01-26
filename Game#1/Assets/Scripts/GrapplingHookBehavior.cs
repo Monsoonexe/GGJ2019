43d69fbfd9b8343947bcc39b73a162b8d265c347
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-01-26 11:39:29 -0800

    Integrated Grappling Hook
    no physics

--- Game#1/Assets/Scripts/GrapplingHookBehavior.cs
+++ Game#1/Assets/Scripts/GrapplingHookBehavior.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class GrapplingHookBehavior : MonoBehaviour
 {
     [SerializeField] private Transform grappleOriginTransform;
+    [SerializeField] private float retractSpeedIncreaseRate = 1.1f;
 
+    private bool isGrappling;
+    private Vector3 retractDirection;
+
+    private Camera playerCam;
     private LineRenderer lineRenderer;
-    private readonly string grapplingHookButton = "GrapplingHook";
+    private Animator animator;
+
+    //private Coroutine coroutine_removeGrapplingHookAfter
+
+    private readonly string grapplingHookButton = "Fire1";
+    private readonly static int maxGrappleHookDistance = 50;
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>() as LineRenderer;
+        playerCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
     }
 
 
@@ -25,11 +36,84 @@ public class GrapplingHookBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //check for grappling hook
+        if (Input.GetButtonDown(grapplingHookButton))
+        {
+            ShootGrapplingHook();
+        }
+        else if (Input.GetButtonUp(grapplingHookButton))
+        {
+            ReleaseGrapplingHook();
+        }
+
+        if (isGrappling)
+        {
+            HandleGrappling();
+        }
     }
 
-    private void GrapplingHook()
+    private void ReleaseGrapplingHook()
     {
+        //release the Hoook
+        isGrappling = false;
+        //remove line renderer
+        lineRenderer.positionCount = 0;
+    }
+
+    private void HandleGrappling()
+    {
+        //add velocity in this direction
+
+        //update line renderer
+        lineRenderer.SetPosition(0, grappleOriginTransform.position);
+        animator.SetBool("Down", true);
+
+    }
+
+    private void ShootGrapplingHook()
+    {
+        //get mouse position in the world
+        Vector3 cursorClick = playerCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 originPoint = grappleOriginTransform.position;
+        retractDirection = cursorClick - originPoint;
+
+        RaycastHit hitInfo;
+
+        //raycast to this point
+        if (Physics.Raycast(originPoint, retractDirection, out hitInfo, maxGrappleHookDistance))
+        {
+            //switch on collider's tag to do specific things
+            switch (hitInfo.collider.gameObject.tag)
+            {
+                //if enemy
+                case "Enemy":
+                    Debug.Log("Grappling Hook hit enemy! That is all.");
+                    break;
+
+                //if environment
+                case "EnvironmentTile":
+                    Debug.Log("Grappling Hook hit environment!");
+                    break;
+            }
+
+            retractDirection = hitInfo.point - originPoint;//may be redundant, but more precise
+            isGrappling = true;
+
+            //handle line renderer
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, originPoint);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+
+        
+        //if not, mouse position is line renderer endpoint
+        
+        
+        //get direction from current point to endpoint
+        //
+
+        //
+
 
     }
 
