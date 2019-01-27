a904ba558346ac49047fce158b97f4db2adb2a9e
wt383 <mrpon@hotmail.com>
2019-01-26 21:12:20 -0800

    Update backandforthmovement.cs

--- Game#1/Assets/Scripts/backandforthmovement.cs
+++ Game#1/Assets/Scripts/backandforthmovement.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class backandforthmovement : MonoBehaviour
 {
     public float speed = 3f;
+    public Vector2 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //offset = transform.position.x + 2;
+        offset.x = transform.position.x;
+        offset.y = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Mathf.PingPong(Time.time * speed, 2), transform.position.y, transform.position.z);
- 
-            
+        transform.position = new Vector3 (Mathf.PingPong(Time.time, 3)+offset.x, offset.y, transform.position.z);
+        //transform.position = new Vector3(Mathf.PingPong(((Time.time - offset) * speed) + 5, 10), transform.position.y, transform.position.z);
+
     }
 }
